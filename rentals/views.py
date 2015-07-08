e55ac05e50453117bec7f2176cef6390013f85c3
lanet <tlane13@umd.edu>
2015-07-08 09:21:52 -0400

    Added views to things

--- rentals/views.py
+++ rentals/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Movie
 
 def movie_list(request):
-	return render(request, 'rentals/movie_list.html', {})
+	movies = Movie.objects.all().order_by('title')
+	return render(request, 'rentals/movie_list.html', {'movies': movies})
 
 # Create your views here.
